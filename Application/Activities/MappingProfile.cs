61fd036a3c2d8ca20167bb0cb5122bd1ffc8cce6
Oláh Levente <levente.olahx@gmail.com>
2020-04-01 08:12:23 +0200

    End of Section 15

--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -10,7 +10,7 @@ namespace Application.Activities
             CreateMap<Activity, ActivityDto>()
                 .ForMember(d => d.Attendees, o => o.MapFrom(s => s.UserActivities));
             CreateMap<UserActivity, AttendeeDto>()
-                .ForMember(d => d.Useranme, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName));
         }
     }
